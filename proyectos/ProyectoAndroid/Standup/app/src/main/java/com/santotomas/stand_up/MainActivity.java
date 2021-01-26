fdd390fd480da8165fef7edc0d3a2c6795932d24
Ismael Quintana Garcia <ismaelquintana8B@gmail.com>
2021-01-26 01:01:44 -0300

    borrando key

--- proyectos/ProyectoAndroid/Standup/app/src/main/java/com/santotomas/stand_up/MainActivity.java
+++ proyectos/ProyectoAndroid/Standup/app/src/main/java/com/santotomas/stand_up/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mAuth = FirebaseAuth.getInstance();
 
         createRequest();
